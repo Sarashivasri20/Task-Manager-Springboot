86c5339cef8b600cff7521bf4767ef32a6db8a5f
Sarashivasri20 <sarashivasri20@gmail.com>
2025-04-26 17:10:40 +0530

    Initial commit

---
+++ src/main/java/com/example/Task_Manager_SpringBoot/TaskManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Task_Manager_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Task_Manager_SpringBoot/TaskManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Task_Manager_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
